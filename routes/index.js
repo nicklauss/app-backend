90840b65dccbd237a268b0d86421b000489a1ade
Anas DEMNATI <anas.demnati2@gmail.com>
2016-05-06 14:01:59 +0100

    Added routes for new pages

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router
+	.get('/layout', function(req, res, next) {
+	  res.render('layout', { title: 'Express' });
+	})
+	.get('/', function(req, res, next) {
+	  res.render('index', { title: 'Express' });
+	})
+	.get('/register', function(req, res, next) {
+	  res.render('register', { title: 'Express' });
+	})
+	.get('/soumission_publication', function(req, res, next) {
+	  res.render('soumission_publication', { title: 'Express' });
+	})
+	.get('/create_conf', function(req, res, next) {
+	  res.render('create_conf', { title: 'Express' });
+	})
+	.get('/creation_compte', function(req, res, next) {
+	  res.render('creation_compte', { title: 'Express' });
+	})
+	.get('/dashboard', function(req, res, next) {
+	  res.render('dashboard', { title: 'Express' });
+	})
+	.get('/list', function(req, res, next) {
+	  res.render('list', { title: 'Express' });
+	})
+	.get('/forms', function(req, res, next) {
+	  res.render('forms', { title: 'Express' });
+	});
 
 module.exports = router;
