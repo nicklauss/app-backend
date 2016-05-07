3d1b731920720126e44878e48541dbf9518ddc51
Anas DEMNATI <anas.demnati2@gmail.com>
2016-05-07 22:30:42 +0100

    added organizer space routes

--- routes/organizer-space/controller.js
+++ routes/organizer-space/controller.js
@@ -6,12 +6,70 @@ exports.index = (req, res, next) => {
     });
 };
 
+exports.newConfy = (req, res, next) => {
+    res.render('organizer-space/tpl/new-confy.html', {
+        title: 'Organizer Space',
+        confy: {
+            open: true,
+            new: true
+        }
+    });
+};
+
+exports.listConfy = (req, res, next) => {
+    res.render('organizer-space/tpl/list-confy.html', {
+        title: 'Organizer Space',
+        confy: {
+            open: true,
+            list: true
+        }
+    });
+};
+
 exports.listExperts = (req, res, next) => {
     res.render('organizer-space/tpl/list-experts.html', {
-        title: 'Liste Experts',
+        title: 'Organizer Space',
         program: {
             open: true,
             list_experts: true
         }
+    });
+};
+
+exports.listMembres = (req, res, next) => {
+  res.render('organizer-space/tpl/list-membres.html', {
+        title: 'Organizer Space',
+      program: {
+          open: true,
+          list_membres: true
+      }
+  });  
+};
+
+exports.listAuteurs = (req, res, next) => {
+    res.render('organizer-space/tpl/list-auteurs.html', {
+        title: 'Organizer Space',
+        auteurs: true
+    });
+};
+
+exports.listParticipants = (req, res, next) => {
+    res.render('organizer-space/tpl/list-participants.html', {
+        title: 'Organizer Space',
+        participants: true
+    });
+};
+
+exports.badges = (req, res, next) => {
+    res.render('organizer-space/tpl/badges.html', {
+        title: 'Organizer space',
+        badges: true
+    });
+};
+
+exports.mailing = (req, res, next) => {
+    res.render('organizer-space/tpl/mailing.html', {
+        title: 'Organizer space',
+        mailing: true
     });
 };
--- routes/organizer-space/index.js
+++ routes/organizer-space/index.js
@@ -6,6 +6,13 @@ const controller = require('./controller');
 
 router
     .get('/index', controller.index)
-    .get('/list-experts', controller.listExperts);
+    .get('/new-confy', controller.newConfy)
+    .get('/list-confy', controller.listConfy)
+    .get('/list-experts', controller.listExperts)
+    .get('/list-membres', controller.listMembres)
+    .get('/list-auteurs', controller.listAuteurs)
+    .get('/list-participants', controller.listParticipants)
+    .get('/badges', controller.badges)
+    .get('/mailing', controller.mailing);
 
 module.exports = router;
