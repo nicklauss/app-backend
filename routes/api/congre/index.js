ca5935b55559ee24309cf0d038bb73d15b2c6392
Anas DEMNATI <anas.demnati2@gmail.com>
2016-05-22 10:23:30 +0100

    edit congre index

--- routes/api/congre/index.js
+++ routes/api/congre/index.js
@@ -5,7 +5,9 @@ const auth = require('../../core/auth/auth.service');
 const controller = require('./controller');
 
 route
-    .post('/', controller.validateCongre, controller.newCongre);
-//    .put('/', controller.updateCongre);
+    .post('/', controller.validateCongre, controller.newCongre)
+    .put('/:congreId', controller.updateCongre)
+    .delete('/:congreId', controller.deleteCongre)
+    .get('/:congreId', controller.getCongreById);
 
 module.exports = route;
