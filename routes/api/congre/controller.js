a9adf261765ad460ad2c130e8c79ac606754c899
Anas DEMNATI <anas.demnati2@gmail.com>
2016-05-22 10:16:47 +0100

    edit congre api

--- routes/api/congre/controller.js
+++ routes/api/congre/controller.js
@@ -25,13 +25,32 @@ exports.validateCongre = (req, res, next) => {
     }
 };
 
+exports.getCongreById = (req, res, next) => {
+    let congreId = req.params.congreId;
+    
+    congre.findById(congreId)
+        .exec((err, congre) => {
+        if(err || !congre) {
+            return res.send({
+                ok: false,
+                message: 'Congre not found'
+            });
+        }
+        return res.send({
+            ok: true,
+            data: congre
+        });
+    });
+};
+
 exports.newCongre = (req, res, next) => {
     let congre = new Congre({
         nom: req.body.nom,
         edition: req.body.edition,
         date_debut: req.body.date_debut,
         date_fin: req.body.date_fin,
-        email: req.body.email
+        email: req.body.email,
+        created: new Date()
     });
     
     congre.save((err) => {
@@ -47,5 +66,76 @@ exports.newCongre = (req, res, next) => {
                 message: 'Your conference has been successfully created'
             });
         }
+    });
+};
+
+exports.updateCongre = (req, res, next) => {
+    let congreId = req.params.congreId;
+    
+    Congre.findById(congreId)
+        .exec((err, congre) => {
+        if(err || !congre) {
+            return res.send({
+                ok: false,
+                message: 'Publication not found'
+            });
+        }
+        congre.nom = req.body.nom || congre.nom;
+        congre.edition = req.body.edition || congre.edition;
+        congre.organisateur_id = req.body.organisateur_id || congre.organisateur_id;
+        congre.location = req.body.location || congre.location;
+        congre.description = req.body.description || congre.description;
+        congre.date_debut = req.body.date_debut || congre.date_debut;
+        congre.date_fin = req.body.date_fin || congre.date_fin;
+        congre.thematique = req.body.thematique || congre.thematique;
+        congre.description = req.body.description || congre.description;
+        congre.site = req.body.site || congre.site;
+        congre.email = req.body.email || congre.email;
+        congre.phone = req.body.phone || congre.phone;
+        congre.soumission = req.body.soumission || congre.soumission;
+        congre.evaluation = req.body.evaluation || congre.evaluation;
+        congre.finalisation = req.body.finalisation || congre.finalisation;
+        congre.updated = new Date();
+        
+        congre.save((err) => {
+            if(err) {
+                return res.send({
+                    ok: false,
+                    message: 'Error while updating'
+                });
+            }
+            return ree.send({
+                ok: true,
+                data: congre
+            });
+        });
+    });
+};
+
+exports.deleteCongre = (req, res, next) => {
+    let congreId = req.params.congreId;
+    
+    Congre.findById(congreId)
+        .exec((err, congre) => {
+        if(err || !congre) {
+            return res.send({
+                ok: false,
+                message: 'Congre not found'
+            });
+        }
+        congre.deleted = true;
+        congre.updated = new Date();
+        congre.save((err) => {
+            if(err) {
+                return res.send({
+                    ok: false,
+                    message: 'Error while deleting'
+                });
+            }
+            return res.send({
+                ok: true,
+                data: congre
+            });
+        });
     });
 };
