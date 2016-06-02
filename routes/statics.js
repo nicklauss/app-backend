782537e701f7432bc5c78888f4d3d3547bc781e8
Anas DEMNATI <anas.demnati2@gmail.com>
2016-06-02 17:19:18 +0100

    added routes to other spaces

--- routes/statics.js
+++ routes/statics.js
@@ -5,9 +5,24 @@ const route = require('express').Router();
 
 const config = require('../config');
 
-route.get('/', (req, res) => {
-    console.log(path.join(config.root,'public/apps/app-organizer/index.html'));
-    res.sendFile(path.join(config.root,'public/apps/app-organizer/index.html'));
-});
+// To change !
+
+route
+	.get('/', (req, res) => {
+	    console.log(path.join(config.root,'public/apps/app-organizer/index.html'));
+	    res.sendFile(path.join(config.root,'public/apps/app-organizer/index.html'));
+	})
+	.get('/author-app', (req, res) => {
+	    console.log(path.join(config.root,'public/apps/app-author/index.html'));
+	    res.sendFile(path.join(config.root,'public/apps/app-author/index.html'));
+	})
+	.get('/reviewer-app', (req, res) => {
+	    console.log(path.join(config.root,'public/apps/app-reviewer/index.html'));
+	    res.sendFile(path.join(config.root,'public/apps/app-reviewer/index.html'));
+	})
+	.get('/program-app', (req, res) => {
+	    console.log(path.join(config.root,'public/apps/app-program/index.html'));
+	    res.sendFile(path.join(config.root,'public/apps/app-program/index.html'));
+	});
 
 module.exports = route;
