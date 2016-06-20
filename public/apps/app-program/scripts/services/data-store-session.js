add970348ebe5e7a53dda60bbbb4be7a63cf3317
Anas DEMNATI <anas.demnati2@gmail.com>
2016-06-20 02:49:20 +0000

    edit & delete sessions + assign presentations

--- public/apps/app-program/scripts/services/data-store-session.js
+++ public/apps/app-program/scripts/services/data-store-session.js
@@ -9,7 +9,11 @@
 
 	function DataStoreSession($http, $q) {
 		var services = {
-			getSessions : getSessions
+			getSessions : getSessions,
+			newSession : newSession,
+			getSessionById : getSessionById,
+			updateSessionById : updateSessionById,
+			deleteSession : deleteSession
 		};
 
 		return services;
@@ -20,11 +24,67 @@
 			$http.get('api/v1/sessions/congres/' + congreId)
 				.success(function(resp, status) {
 					deferred.resolve(resp);
+					console.log(resp);
 				})
 				.error(function(error, status) {
 					deferred.reject(error);
 				});
 			return deferred.promise;
+		}
+
+		function getSessionById(sessionId) {
+			var deferred = $q.defer();
+
+			$http.get('api/v1/sessions/' + sessionId)
+				.success(function(resp, status) {
+					deferred.resolve(resp);
+					console.log(resp);
+				})
+				.error(function(error, status) {
+					deferred.reject(error);
+				});
+			return deferred.promise;
+		}
+
+		function newSession(sessionObject) {
+			var deferred = $q.defer();
+
+			$http.post('api/v1/sessions', sessionObject)
+				.success(function(resp, status) {
+					deferred.resolve(resp);
+				})
+				.error(function(error, status) {
+					deferred.reject(error);
+				});
+			return deferred.promise;
+		}
+
+		function updateSessionById(presentationObj) {
+			var deferred = $q.defer();
+            console.log(presentationObj);
+
+			$http.put('api/v1/sessions/' + presentationObj._id, presentationObj)
+				.success(function(resp, status) {
+					deferred.resolve(resp);
+					console.log(resp);
+				})
+				.error(function(error, status) {
+					deferred.reject(error);
+				});
+		  return deferred.promise;
+		}
+
+		function deleteSession(sessionId) {
+			var deferred = $q.defer();
+
+			$http.delete('api/v1/sessions/' + sessionId)
+				.success(function(resp, status) {
+					deferred.resolve(resp);
+				})
+				.error(function(error, status) {
+					deferred.reject(error);
+				});
+		  return deferred.promise;
 		}
 
 
