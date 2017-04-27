b8f7f07bd30863d7d114d45c3cf898d836601cbf
anas demnati <anas.demnati2@gmail.com>
2017-04-27 21:03:38 +0100

    correct bug

--- routes/core/auth/index.js
+++ routes/core/auth/index.js
@@ -13,7 +13,7 @@ var router = express.Router();
 router
     .use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
       res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
       next();
     })
